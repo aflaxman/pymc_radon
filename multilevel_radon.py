81b74eae6bedec60815b677e665aaf0339fab9e3
abie <abie@alap.domain.actdsltmp>
2009-12-01 22:46:49 -0800

    add additional csv files and a multilevel radon example

---
+++ multilevel_radon.py
@@ -0,0 +1,34 @@
+import csv
+from numpy import *
+from pymc import *
+
+household_data = [d for d in csv.DictReader(open('household.csv'))]
+county_data = [d for d in csv.DictReader(open('county.csv'))]
+
+# hyper-priors
+g = Uniform('gamma', [0,0], [100,100])
+
+s_a = Uniform('sigma_a', 0, 100)
+
+a = {}
+for d in county_data:
+    @stochastic(name='a_%s'%d['county'])
+    def a_j(value=0., g=g, u_j=float(d['u']), s_a=s_a):
+        return normal_like(value, g[0] + g[1]*u_j, s_a**-2.)
+    a[d['county']] = a_j
+
+# priors
+b = Uniform('beta', 0, 100)
+
+s_y = Uniform('sigma_y', 0, 100)
+
+# likelihood
+y = {}
+for d in household_data:
+    @stochastic(observed=True, name='y_%s'%d['household'])
+    def y_i(value=float(d['y']), a_j=a[d['county']], b=b,
+            x_ij=float(d['x']), s_y=s_y):
+        return normal_like(value, a_j + b*x_ij, s_y**-2.)
+    y[d['household']] = y_i
+
+mc = MCMC([g, s_a, a, b, s_y, y])
