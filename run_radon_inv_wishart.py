a17b33007506f3a772e34883d4bbffe1133890a7
unknown <abie@.clients.nebula2.washington.edu>
2010-01-27 11:40:40 -0800

    functionize the run module, so that I can time it easily in ipython
    >>> time run_radon.run('small')
    ('mu', array([ 1.46223278, -0.69145453]))
    ('xi', array([ 1.13091177,  0.76375119]))
    ('sigma_y', 0.75684556225644739)
    ('tau_y', 1.7479352273104205)
    CPU times: user 45.47 s, sys: 0.00 s, total: 45.47 s
    Wall time: 45.47

--- run_radon_inv_wishart.py
+++ run_radon_inv_wishart.py
@@ -6,26 +6,30 @@ from pylab import hist, show
 from pymc import Matplot
 from timeit import Timer
 
-M = pymc.MCMC(radon_inv_wishart)
+def run(size='small'):
+    M = pymc.MCMC(radon_inv_wishart)
 
-## small
-M.sample(iter=1e3, burn=500, thin=5)
+    if size == 'small':
+        M.sample(iter=1e3, burn=500, thin=5)
 
-## med
-#M.sample(iter=5e3, burn=1e3, thin=10)
+    elif size == 'medium':
+        M.sample(iter=5e3, burn=1e3, thin=10)
 
-## large
-#M.sample(iter=10e3, burn=5e3, thin=10)
+    elif size == 'large':
+        M.sample(iter=10e3, burn=5e3, thin=10)
 
-fit = M.stats()
-print('mu',fit['mu']['mean'])
-print('xi',fit['xi']['mean'])
-print('sigma_y',fit['sigma_y']['mean'])
-print('tau_y',fit['tau_y']['mean'])
+    else:
+        raise Error, 'unrecognized size: %s' % size
 
-B = M.trace('B')[:]
-B = sum(B)/len(B)
-outf = open('radon.coefs.from.pymc.csv','w')
-coefsWriter = csv.writer(outf)
-coefsWriter.writerows(B)
-outf.close()
+    fit = M.stats()
+    print('mu',fit['mu']['mean'])
+    print('xi',fit['xi']['mean'])
+    print('sigma_y',fit['sigma_y']['mean'])
+    print('tau_y',fit['tau_y']['mean'])
+
+    B = M.trace('B')[:]
+    B = sum(B)/len(B)
+    outf = open('radon.coefs.from.pymc.csv','w')
+    coefsWriter = csv.writer(outf)
+    coefsWriter.writerows(B)
+    outf.close()
