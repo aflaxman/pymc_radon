3c1ae693fc3fda8151f95859ce10899d0f848d44
unknown <abie@.clients.nebula2.washington.edu>
2010-01-27 12:40:55 -0800

    notes on potential speedups
    i predict vectorizing MvNormal will be the big improvement

--- radon_inv_wishart.py
+++ radon_inv_wishart.py
@@ -46,24 +46,28 @@ xi = pymc.Uniform('xi', lower=0, upper=100, value=np.zeros(K))
 mu_raw = pymc.Normal('mu_raw', mu=0., tau=0.0001,value=np.zeros(K))
 Tau_B_raw = pymc.Wishart('Tau_B_raw', df, Tau=np.diag(np.ones(K)))
 B_raw_m = np.ones( (J,K) )
+
+# TODO: vectorize MvNormal if possible
 B_raw = []
 for i in range(J):
     B_raw.append(pymc.MvNormal('B_raw_%i' % i, mu_raw, Tau_B_raw))
 
-@pymc.deterministic
-def Sigma_B_raw(Tau_B_raw=Tau_B_raw):
-    return np.linalg.inv(Tau_B_raw)
+# TODO: determine if the following 3 vars are necessary
+# @pymc.deterministic
+# def Sigma_B_raw(Tau_B_raw=Tau_B_raw):
+#     return np.linalg.inv(Tau_B_raw)
 
-@pymc.deterministic
-def rho_B(Sigma_B_raw=Sigma_B_raw):
-    return Sigma_B_raw / np.sqrt(np.diag(Sigma_B_raw) * Sigma_B_raw)
+# @pymc.deterministic
+# def rho_B(Sigma_B_raw=Sigma_B_raw):
+#     return Sigma_B_raw / np.sqrt(np.diag(Sigma_B_raw) * Sigma_B_raw)
 
-@pymc.deterministic
-def Sigma_B(xi=xi,Sigma_B_raw=Sigma_B_raw):
-    return abs(xi) * np.sqrt(np.diag(Sigma_B_raw))
+# @pymc.deterministic
+# def Sigma_B(xi=xi,Sigma_B_raw=Sigma_B_raw):
+#     return abs(xi) * np.sqrt(np.diag(Sigma_B_raw))
 
 @pymc.deterministic(plot=True)
 def B(xi=xi, B_raw=B_raw, B_raw_m=B_raw_m):
+    # FIXME: what is the effect of this loop?  perhaps it is not necessary
     for i in range(J):
         B_raw_m[i] = B_raw[i]
     return xi * B_raw_m
@@ -75,7 +79,7 @@ def mu(xi=xi, mu_raw=mu_raw):
 # Model
 @pymc.deterministic(plot=False)
 def y_hat(B=B):
-       return (B[index_c,] *X).sum(axis=1)
+       return (B[index_c,] *X).sum(axis=1)  # TODO: try dot product instead of element-wise prod and sum
 
 # Likelihood
 @pymc.stochastic(observed=True)
